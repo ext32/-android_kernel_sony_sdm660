0cde35f309b1758a7905371141ea4aa43f22d773
Daniel Lezcano <daniel.lezcano@linaro.org>
2016-10-05 09:33:12 +0200

    cpuidle: governors: Remove remaining old module code
    The governor's code use try_module_get() and put_module() to refcount
    the governor's module. But the governors are not compiled as module.
    
    The refcount does not prevent to switch the governor or unload
    a module as they aren't compiled as modules. The code is pointless,
    so remove it.
    
    Signed-off-by: Daniel Lezcano <daniel.lezcano@linaro.org>
    Signed-off-by: Rafael J. Wysocki <rafael.j.wysocki@intel.com>
    Signed-off-by: celtare21 <celtare21@gmail.com>

--- drivers/cpuidle/governors/menu.c
+++ drivers/cpuidle/governors/menu.c
@@ -20,7 +20,6 @@
 #include <linux/sched.h>
 #include <linux/math64.h>
 #include <linux/cpu.h>
-#include <linux/module.h>
 
 /*
  * Please note when changing the tuning values:
@@ -502,7 +501,6 @@ static struct cpuidle_governor menu_governor = {
 	.enable =	menu_enable_device,
 	.select =	menu_select,
 	.reflect =	menu_reflect,
-	.owner =	THIS_MODULE,
 };
 
 /**
