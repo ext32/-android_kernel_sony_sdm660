7acff88140e8978da6122eedf19fca7a3b0ac851
Ext3 <ext3@testbed>
2022-08-20 11:53:02 +0300

    mm: userfaultfd: fix missing cache flush in mcopy_atomic_pte() and __…

--- mm/userfaultfd.c
+++ mm/userfaultfd.c
@@ -50,6 +50,7 @@ static int mcopy_atomic_pte(struct mm_struct *dst_mm,
 			/* don't free the page */
 			goto out;
 		}
+		flush_dcache_page(page);
 	} else {
 		page = *pagep;
 		*pagep = NULL;
@@ -264,6 +265,7 @@ retry:
 				err = -EFAULT;
 				goto out;
 			}
+			flush_dcache_page(page);
 			goto retry;
 		} else
 			BUG_ON(page);
