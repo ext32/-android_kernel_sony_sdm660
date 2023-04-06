e10960b09a21e116e268804b81944b1f6a7f0b0c
Ext3 <ext3@testbed>
2023-04-06 13:36:34 +0300

    msm: kgsl: Fix drawqueue timer race condition

--- drivers/gpu/msm/adreno_dispatch.c
+++ drivers/gpu/msm/adreno_dispatch.c
@@ -646,21 +646,6 @@ static int sendcmd(struct adreno_device *adreno_dev,
 		return ret;
 	}
 
-	secs = time.ktime;
-	nsecs = do_div(secs, 1000000000);
-
-	trace_adreno_cmdbatch_submitted(drawobj, (int) dispatcher->inflight,
-		time.ticks, (unsigned long) secs, nsecs / 1000, drawctxt->rb,
-		adreno_get_rptr(drawctxt->rb));
-
-	mutex_unlock(&device->mutex);
-
-	cmdobj->submit_ticks = time.ticks;
-
-	dispatch_q->cmd_q[dispatch_q->tail] = cmdobj;
-	dispatch_q->tail = (dispatch_q->tail + 1) %
-		ADRENO_DISPATCH_DRAWQUEUE_SIZE;
-
 	/*
 	 * For the first submission in any given command queue update the
 	 * expected expire time - this won't actually be used / updated until
@@ -671,6 +656,22 @@ static int sendcmd(struct adreno_device *adreno_dev,
 	if (dispatch_q->inflight == 1)
 		dispatch_q->expires = jiffies +
 			msecs_to_jiffies(adreno_drawobj_timeout);
+
+	secs = time.ktime;
+        nsecs = do_div(secs, 1000000000);
+
+        trace_adreno_cmdbatch_submitted(drawobj, (int) dispatcher->inflight,
+                time.ticks, (unsigned long) secs, nsecs / 1000, drawctxt->rb,
+                adreno_get_rptr(drawctxt->rb));
+
+        mutex_unlock(&device->mutex);
+
+        cmdobj->submit_ticks = time.ticks;
+
+        dispatch_q->cmd_q[dispatch_q->tail] = cmdobj;
+        dispatch_q->tail = (dispatch_q->tail + 1) %
+                ADRENO_DISPATCH_DRAWQUEUE_SIZE;
+
 
 	/*
 	 * If we believe ourselves to be current and preemption isn't a thing,
